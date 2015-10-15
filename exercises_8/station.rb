f0247866675b49ba8433bb2b9947781c650fe8df
AndreyPerekosov <perekosov@gmail.com>
2015-10-15 13:27:54 +0300

    home_task_8

--- exercises_8/station.rb
+++ exercises_8/station.rb
@@ -1,64 +1,64 @@
 class Station
-	
-	attr_reader :name, :table
-	
-	@@names = []
-	NAME_FORMAT = /\A[a-z]+\Z/i
+  
+  attr_reader :name, :table
+  
+  @@names = []
+  NAME_FORMAT = /\A[a-z]+\Z/i
 
-	def self.all
-		@@names.each {|name| puts "#{name}"}
-	end
+  def self.all
+    @@names.each {|name| puts "#{name}"}
+  end
 
 
-	def initialize(name)
-			@name = name
-			validate!
-			@table = []
-			@@names << name	
-	end
-		
-	def valid?
-		validate!
-	rescue
-		false
-	end
+  def initialize(name)
+      @name = name
+      validate!
+      @table = []
+      @@names << name 
+  end
+    
+  def valid?
+    validate!
+  rescue
+    false
+  end
 
   # Метод обработки поездов на станции принимающий блок
-	def trains_handler(&block)
+  def trains_handler(&block)
     table.each do |object|
       block.call(object)
     end
   end
 
-	end
+  end
 
-	def train_in (train)
-			table << train
-	end
+  def train_in (train)
+      table << train
+  end
 
-	def train_out (train)
-		table.delete[train]
-	end
+  def train_out (train)
+    table.delete[train]
+  end
 
-	def show_train_tables
-		puts " Cargo train: #{table_sort!("cargo")} Sum: #{table_sort!(cargo).lenght}"
-		puts " Passenger train: #{table_sort!("passenger")} Sum: #{table_sort!(passenger).lenght}"
-	end
+  def show_train_tables
+    puts " Cargo train: #{table_sort!("cargo")} Sum: #{table_sort!(cargo).lenght}"
+    puts " Passenger train: #{table_sort!("passenger")} Sum: #{table_sort!(passenger).lenght}"
+  end
 
 
-	private
-	
-	attr_writer :table
-	
-	def table_sort!(type)
-		hashe = {}
-		table.each do |train|
-			hashe[train.number] = type if train.type == type
-		end 
-		hashe	
-	end
-		def validate!
-			raise ArgumentError, "Wrong name" if name !~ NAME_FORMAT
-			true
-		end
+  private
+  
+  attr_writer :table
+  
+  def table_sort!(type)
+    hashe = {}
+    table.each do |train|
+      hashe[train.number] = type if train.type == type
+    end 
+    hashe 
+  end
+    def validate!
+      raise ArgumentError, "Wrong name" if name !~ NAME_FORMAT
+      true
+    end
 end
--- exercises_8/train.rb
+++ exercises_8/train.rb
@@ -1,115 +1,115 @@
 require_relative 'module_company'
 
 class Train 
-	
-	include Company
+  
+  include Company
 
-	attr_reader :number, :railcars, :speed, :route
-	
-	@@numbers = {}
+  attr_reader :number, :railcars, :speed, :route
+  
+  @@numbers = {}
 
-	NUMBER_FORMAT = /\A[a-z0-9]{3}[-]{0,1}[a-z0-9]{2}\Z/i
+  NUMBER_FORMAT = /\A[a-z0-9]{3}[-]{0,1}[a-z0-9]{2}\Z/i
 
-	def self.find(number)
-		@@numbers[number]
-	end
+  def self.find(number)
+    @@numbers[number]
+  end
 
 
-	def initialize (number, speed = 0.0)
-		@number = number
-		@railcars = []
-		@speed = speed
-		@@numbers[number] = self
-		validate!
-		@route = []
-		@waypoint = 0
-	end
+  def initialize (number, speed = 0.0)
+    @number = number
+    @railcars = []
+    @speed = speed
+    @@numbers[number] = self
+    validate!
+    @route = []
+    @waypoint = 0
+  end
 
-	def valid?
-		validate!
-	rescue
-		false
-	end
+  def valid?
+    validate!
+  rescue
+    false
+  end
 
-	# Метод обработки вагонов поезда принимающий блок	
-	def railcars_handler(&block)
-		railcars.each do |object|
+  # Метод обработки вагонов поезда принимающий блок 
+  def railcars_handler(&block)
+    railcars.each do |object|
       block.call(object)
     end
-	end
+  end
 
 
-	def speed_up
-		self.speed += 10
-		if speed > 110 
-			self.speed = 110.0
-		end
-	end
+  def speed_up
+    self.speed += 10
+    if speed > 110 
+      self.speed = 110.0
+    end
+  end
 
-	def speed_down
-		self.speed -= 10 
-		if speed < 0 
-			self.speed = 0
-		end
-	end
-	
-	def route(route)
-		self.route = route
-	end
+  def speed_down
+    self.speed -= 10 
+    if speed < 0 
+      self.speed = 0
+    end
+  end
+  
+  def route(route)
+    self.route = route
+  end
 
-	def move_forward
-		self.waypoint += 1 if waypoint < (route.size - 1)
-	end
+  def move_forward
+    self.waypoint += 1 if waypoint < (route.size - 1)
+  end
 
-	def move_reverse
-		self.waypoint -= 1 if waypoint > 0 
-	end
+  def move_reverse
+    self.waypoint -= 1 if waypoint > 0 
+  end
 
-	def current_station
-		route[waypoint].name
-	end
-	
-	def previous_station
-		if waypoint > 0
-		 route[waypoint - 1].name 
-		end
-	end
+  def current_station
+    route[waypoint].name
+  end
+  
+  def previous_station
+    if waypoint > 0
+     route[waypoint - 1].name 
+    end
+  end
 
-	def next_station
-		if @waypoint < (@route.size - 1)
-			route[waypoint + 1].name
-		end
-	end
+  def next_station
+    if @waypoint < (@route.size - 1)
+      route[waypoint + 1].name
+    end
+  end
 
-	def railcar_unhook
-		if train_stoped?
-			railcars.delete(railcars.size - 1)
-		end
-	end
+  def railcar_unhook
+    if train_stoped?
+      railcars.delete(railcars.size - 1)
+    end
+  end
 
 
 protected
 
 attr_writer :railcars, :speed, :route, :waypoint, :number
 
-	def train_stoped?
-		speed == 0
-	end
+  def train_stoped?
+    speed == 0
+  end
 
 
-	def railcar_hook!(railcar)
-		if train_stoped?
-			railcars << railcar
-			railcar.number = railcars.size
-		end
-	end
+  def railcar_hook!(railcar)
+    if train_stoped?
+      railcars << railcar
+      railcar.number = railcars.size
+    end
+  end
 
-	def validate!
-		raise ArgumentError, "Speed can't be negative" if speed < 0
-		raise "This train already exists" if @@numbers[number]
-		raise ArgumentError, "Wrong number" if number !~ NUMBER_FORMAT 
-		true
-	end
+  def validate!
+    raise ArgumentError, "Speed can't be negative" if speed < 0
+    raise "This train already exists" if @@numbers[number]
+    raise ArgumentError, "Wrong number" if number !~ NUMBER_FORMAT 
+    true
+  end
 
 
 end
