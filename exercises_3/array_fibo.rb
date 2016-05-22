ff63cbeefa8812027514d2e30d46efbdd765f17d
PerekosovAndrey <perekosov@gmail.com>
2016-05-22 23:42:16 +0300

    some fixes for lesson 3

--- exercises_3/array_fibo.rb
+++ exercises_3/array_fibo.rb
@@ -2,7 +2,7 @@ a = [0, 1]
 init = init_2 = 0
 init_1 = 1
 
-loop do init > 100
+loop do 
 	init = init_1 + init_2
 	break if init > 100
 	a << init 
--- exercises_3/hash_of_vowels.rb
+++ exercises_3/hash_of_vowels.rb
@@ -1,12 +1,30 @@
-letters = ("a".."z").to_a
-array_vow = letters.select {|l| l =~ /[euioa]/}
+# letters = ("a".."z").to_a
+# #array_vow = letters.select {|l| l =~ /[euioa]/}
+# hash_vow = {}
 
+# #array_vow.each do |v|
+# 	hash_vow[v.to_sym] = i
+# 	i += 1 
+# end
+# hash_vow.each do |v, k|
+# 	puts "#{v}: #{k}"
+# end
+letters  = ("a".."z")
+array_vow = ["e", "u", "i", "o", "a"]
 hash_vow = {}
 i = 0
-array_vow.each do |v|
-	hash_vow[v.to_sym] = i
-	i += 1 
+
+# letters.each do |letter|
+#   i +=1
+#   array_vow.each do |vow|
+#     hash_vow[letter] = i if vow == letter
+#   end
+# end
+#Вариант работает но можно еще с помощью метода include?
+
+letters.each do |letter|
+  i+=1
+  hash_vow[letter] = i if array_vow.include?(letter)
 end
-hash_vow.each do |v, k|
-	puts "#{v}: #{k}"
-end
+
+hash_vow.each { |k, v| puts "#{k}: #{v}"  }
